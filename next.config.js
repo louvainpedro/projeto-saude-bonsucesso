4cb6922bacd1f36b30147604ed5b6876ccf7502c
louvainpedro <pedro.louvain@lymtech.com.br>
2024-10-15 00:44:48 -0300

    fix: consertando next.config.js para aceitar img

--- next.config.js
+++ next.config.js
@@ -3,6 +3,9 @@ const nextConfig = {
     basePath: '/psb', // Ajuste conforme necessário
     output: 'export',
     reactStrictMode: true,
+    images: {
+      unoptimized: true,
+    }
   };
   
   module.exports = nextConfig;
