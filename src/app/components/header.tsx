f15d9affa5fb05f8893339bf59d95eb7d53d7d26
louvainpedro <pedro.louvain@lymtech.com.br>
2024-10-15 01:19:21 -0300

    fix: consertando logo

--- src/app/components/header.tsx
+++ src/app/components/header.tsx
@@ -17,7 +17,7 @@ export default function Header ({  onSearch }: HeaderProps) {
       <img
         src="images/logoProjetoSaude.png"
         alt="Company Logo"
-        className="h-18 my-auto"
+        className="h-20 my-auto"
       />
       <div className="flex h-full items-center text-black">
         <input
